70121725ad278f55de1cf830847d3d9b17a90820
gelin <gllovejava@163.com>
2018-09-25 22:39:53 +0800

    create

---
+++ src/main/java/com/gelin/activitispringboot/ActivitiSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.gelin.activitispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiSpringbootApplication.class, args);
+    }
+}
