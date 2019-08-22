ad5a02820a0e7faaf5a915540712fe861ef5f9d7
green <962422150@qq.com>
2019-08-22 17:46:09 +0800

    子流程

---
+++ src/test/java/com/gelin/activitispringboot/call/Call.java
@@ -0,0 +1,71 @@
+package com.gelin.activitispringboot.call;
+
+import com.gelin.activitispringboot.ActivitiSpringbootApplication;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ActivitiSpringbootApplication.class)
+public class Call {
+
+    @Resource
+    private RepositoryService repositoryService;
+    @Resource
+    private RuntimeService runtimeService; // 与正在执行的流程实例和执行对象相关的
+    @Resource
+    private TaskService taskService;
+
+    // 流程部署
+    @Test
+    public void deploySimpleProcessDefinition() {
+        Deployment deploy = repositoryService.createDeployment()
+                .name("子流程")
+                .addClasspathResource("processes/call/call.bpmn")
+                .deploy();
+        System.out.println(deploy);
+    }
+
+    // 启动流程实例
+    @Test
+    public void startProcess(){
+        ProcessInstance helloworld = runtimeService.startProcessInstanceByKey("callProcess");// 使用 key 来启动好处 默认寻找最新版本，版本多了的话
+    }
+
+    @Test
+    public void findMyTask(){
+        List<Task> taskList = taskService.createTaskQuery() // 创建人物查询对象
+                                         .taskCandidateUser("葛林1") //指定个人任务查询
+                                         .list();
+        if(taskList != null && taskList.size()>0){
+            for (Task task:taskList) {
+                System.out.println("任务ID:"+task.getId());
+                System.out.println("任务名称:"+task.getName());
+                System.out.println("任务的创建时间:"+task.getCreateTime());
+                System.out.println("任务的办理人:"+task.getAssignee());
+                System.out.println("流程实例ID:"+task.getProcessInstanceId());
+                System.out.println("执行对象ID:"+task.getExecutionId());
+                System.out.println("执行定义ID:"+task.getProcessDefinitionId());
+            }
+        }
+    }
+
+    // 完成我的任务
+    @Test
+    public void completeMyTask(){
+        // 需要任务id
+        taskService.complete("25005");
+        System.out.println("任务完成");
+    }
+
+}
